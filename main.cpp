e16764e53f57d08c7a592fac64a83b2db07e112e
aleksander73 <olekdobkowski@gmail.com>
2021-01-23 20:39:49 +0100

    Example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+#include "Vector2d.h"
+#include "HillClimber.h"
+
+float f(Vector2d v) {
+	return -(v.getX() * v.getX() + v.getY() * v.getY());
+}
+
 int main(int argc, char* argv[]) {
-	std::cout << "Hello world!" << std::endl;
+	HillClimber hillClimber = HillClimber(-100.0f, 100.0f, -100.0f, 100.0f, 100, 0.01, 100);
+	Vector2d maximum = hillClimber.findMaximum(&f);
+	std::cout << "Maximum: " << maximum << " = " << f(maximum) << std::endl;
 	return 0;
 }
