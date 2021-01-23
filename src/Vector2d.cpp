137847f8301a2c1551fd705f28d4fb78562b1100
aleksander73 <olekdobkowski@gmail.com>
2021-01-23 17:40:01 +0100

    Implemented Vector2d::rotate() function

--- src/Vector2d.cpp
+++ src/Vector2d.cpp
@@ -1,6 +1,7 @@
 #include "Vector2d.h"
 
 #include <math.h>
+#include <cmath>
 
 Vector2d::Vector2d(float x, float y) {
 	this->x = x;
@@ -9,6 +10,14 @@ Vector2d::Vector2d(float x, float y) {
 
 Vector2d Vector2d::operator+(Vector2d& v) {
 	return Vector2d(this->x + v.x, this->y + v.y);
+}
+
+Vector2d Vector2d::rotate(float angle) {
+	float angle_ = fmod(angle, 360.0f);
+	float rad = angle_ * M_PI / 180.0f;
+	float sin = std::sin(rad);
+	float cos = std::cos(rad);
+	return Vector2d(this->x * cos - this->y * sin, this->x * sin + this->y * cos);
 }
 
 float Vector2d::getX() {
